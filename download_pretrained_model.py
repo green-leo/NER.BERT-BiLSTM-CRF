e4b3e7d136414cba30cf788cd34f6231da0dc0ff
GreenLeo <nguyetnguyen15802@gmail.com>
2022-03-15 21:34:37 +0700

    Update download_pretrained_model.py

--- download_pretrained_model.py
+++ download_pretrained_model.py
@@ -1,6 +1,5 @@
-from transformers import AutoModel, AutoTokenizer # Thư viện BERT
+from transformers import AutoModel, AutoTokenizer
 
-# Load vinai phobert-base
 bert = AutoModel.from_pretrained("bert-base-uncased")
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", use_fast=False)
 bert.save_pretrained("./pretrained_model/bert_base_uncased")
